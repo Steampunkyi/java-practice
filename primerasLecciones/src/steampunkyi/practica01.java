7cc6905967e886cc3df7cca9a00446f48b4216eb
steampunkyi <u20219834@utp.edu.pe>
2021-10-27 14:52:58 -0500

    Añadido la tarea de la semana 11

--- primerasLecciones/src/steampunkyi/practica01.java
+++ primerasLecciones/src/steampunkyi/practica01.java
@@ -1,5 +1,17 @@
 package steampunkyi;
 
-public class practica01 {
+import javax.swing.JOptionPane;
 
+public class practica01 {
+    public static void main(String[] args) {
+        String dato = JOptionPane.showInputDialog(null, "Dame un numero positivo: ");
+        int numUno = Integer.parseInt(dato);
+        String par = "";
+        if (numUno % 2 == 0) {
+            par = par + "El numero " + numUno + " es par";
+        } else {
+            par = par + "El numero " + numUno + " es impar";
+        }
+        JOptionPane.showMessageDialog(null, par, "Que numero es?", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
